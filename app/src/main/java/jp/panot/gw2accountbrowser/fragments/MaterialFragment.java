884b0216c07fcf49c894f5952c3e0cdb65915a41
Panot Chaimongkol <mel.sk125@gmail.com>
2016-02-29 15:26:52 +0900

    Add skeleton for new MaterialFragment

---
+++ app/src/main/java/jp/panot/gw2accountbrowser/fragments/MaterialFragment.java
@@ -0,0 +1,10 @@
+package jp.panot.gw2accountbrowser.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by panot on 2/29/16.
+ */
+public class MaterialFragment extends Fragment {
+  private static final String LOG_TAG = MaterialFragment.class.getSimpleName();
+}
